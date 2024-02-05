9814fda2203655f8b1159c0cffa939b11a8b0ee8
vanji <vikashvanchi@gmail.com>
2024-02-05 20:14:02 +0530

    The compressed image will be of 16*32 size now

--- blockDivision.m
+++ blockDivision.m
@@ -10,6 +10,10 @@ img = imread(imagePath);
 % Specify the block size
 blockSize = 32;
 
+% Calculate the number of blocks in each dimension
+numBlocksX = floor(width / blockSize);
+numBlocksY = floor(height / blockSize);
+
 % Create a directory to store processed images
 processedDir = 'D:\imageDecryption-CSRNET\processed_images';
 if ~exist(processedDir, 'dir')
@@ -34,11 +38,11 @@ for i = 1:numBlocksY
         % Perform DCT on the grayscale block
         dctBlock = dct2(grayBlock);
         
-        % Create a random binary matrix of size 16x32
-        binaryMatrix = randi([0, 1], 16, 32);
+        % Create a random binary matrix of size 32x16
+        binaryMatrix = randi([0, 1], 32, 16);
         
         % Multiply the DCT block with the random binary matrix
-        result = binaryMatrix .* dctBlock(1:16, 1:32);
+        result = dctBlock * binaryMatrix ;
         
         % Generate a unique filename for each block
         filename = fullfile(processedDir, sprintf('block_%d_%d.png', i, j));
@@ -50,5 +54,3 @@ end
 
 % Create imageDatastore for the processed images
 imds = imageDatastore(processedDir, 'LabelSource', 'none');
-
-
